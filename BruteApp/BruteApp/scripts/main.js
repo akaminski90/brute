d70894b20be77777473ed99cbaf4c364fe16b8ab
Alexander Kaminski <akaminski@dev-team.com>
2017-09-27 13:00:35 +0300

    Create new project, instulled Umbraco CMS, istalled usync package

---
+++ BruteApp/BruteApp/scripts/main.js
@@ -0,0 +1,10 @@
+// $('.bars').click(function () {
+//     $('.mobile-menu').addClass('active');
+//     $('.wrapper').addClass('mobile-menu-active');
+// });
+//
+// $('.close').click(function () {
+//     $('.mobile-menu').removeClass('active');
+//     $('.wrapper').removeClass('mobile-menu-active');
+// });
+
