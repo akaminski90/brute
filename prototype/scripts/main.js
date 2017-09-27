435f0a77d1786463d39d420ff59264c4d2dd9bde
Alexander Kaminski <akaminski@dev-team.com>
2017-09-27 16:40:01 +0300

    Added arrows script in vanilla JS

--- prototype/scripts/main.js
+++ prototype/scripts/main.js
@@ -12,21 +12,32 @@
 /*********************SIDEBAR****************************************************/
 var sidebar = document.querySelector('.sidebar');
 var article = document.querySelector('body article');
+var siteName = document.querySelector('.navbar ul');
 var sidebarStyle = sidebar.currentStyle || window.getComputedStyle(sidebar);
 document.querySelector('.bars').addEventListener('click', function () {
     if (sidebarStyle.left === '0px' || sidebarStyle.left === '') {
         sidebar.style.left = '-300px';
         article.style.marginLeft = '0px';
+        siteName.style.marginLeft = '50px';
     } else {
         sidebar.style.left = '0px';
         article.style.marginLeft = '300px';
+        siteName.style.marginLeft = '250px';
     }
 });
 
 /**********************ARROWS******************************************************/
 
-$('.br-down').click(function () {
+/*$('.br-down').click(function () {
     var $li = $(this).parent().parent();
     $li.toggleClass('active');
     $(this).toggleClass('br-down').toggleClass('br-up');
+});*/
+
+document.body.addEventListener('click', function (event) {
+    if (event.target.classList.contains('br-down') || event.target.classList.contains('br-up')) {
+        event.target.classList.toggle('br-down');
+        event.target.classList.toggle('br-up');
+        event.target.parentNode.parentNode.classList.toggle('active');
+    }
 });
