3ef6e202c7e94432534fb31e158c24f5ecef5f78
Alexander Kaminski <akaminski@dev-team.com>
2017-10-12 12:32:36 +0300

    Added changes from prototype

--- BruteApp/BruteApp/Controllers/MusicianSurfaceController.cs
+++ BruteApp/BruteApp/Controllers/MusicianSurfaceController.cs
@@ -20,7 +20,7 @@ namespace BruteApp.Controllers
             //var start = count;
             var musicians = Umbraco.TypedContentAtXPath("//musician");
             musicians = musicians.OrderBy(x => x.GetPropertyValue("navigationName")).Skip(count).Take(6);
-            string str = musicians.Aggregate("", (current, musician) => current + ("<div class='col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-2'><a href='" + musician.Url.ToString() + "' class='musician'><img class='lazy' data-src='" + Url.GetCropUrl(musician, "image", "musicianCropper").ToString() + "'><div class='description'><p>" + musician.GetPropertyValue("navigationName").ToString() + "</p><span>перевод песен</span></div></a></div>"));
+            var str = musicians.Aggregate("", (current, musician) => current + ("<div class='col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-2'><a href='" + musician.Url.ToString() + "' class='musician'><img class='lazy' data-src='" + Url.GetCropUrl(musician, "image", "musicianCropper").ToString() + "'><div class='description'><p>" + musician.GetPropertyValue("navigationName").ToString() + "</p><span>перевод песен</span></div></a></div>"));
 
 
             return Json(str, JsonRequestBehavior.AllowGet);
