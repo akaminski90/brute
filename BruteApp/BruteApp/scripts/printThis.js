54584b84dfb9f764236d416ce44b9aad5397aef5
Alexander Kaminski <akaminski@dev-team.com>
2017-10-20 13:23:56 +0300

    Fixed script bug in IE

--- BruteApp/BruteApp/scripts/printThis.js
+++ BruteApp/BruteApp/scripts/printThis.js
@@ -10,57 +10,57 @@ function openPrintDialogue() {
     $iframe
         .contents()
         .find('head')
-        .append(`
-    <style>
-            h1 {
-                text-align: center;
-                display: block;
-                font-size: 25px;
-            }
+        .append(
+    "<style>"
+            + "h1 {"
+                + "text-align: center;"
+                + "display: block;"
+                + "font-size: 25px;"
+            +"}"
 
-            .for-print {
-                text-align: center;
-            display: block;
-            font-size: 22px;
-            font-weight: 600;
-            margin: 25px 0;
-        }
+            + ".for-print {"
+            + "text-align: center;"
+            + "display: block;"
+            + "font-size: 22px;"
+            + "font-weight: 600;"
+            + "margin: 25px 0;"
+        + "}"
 
-        .text {
-            font-size: 16px;
-        }
+        + ".text {"
+            + "font-size: 16px;"
+        + "}"
 
-        .text .string {
-            border-bottom: 1px solid #eee;
-            padding: 5px 0;
-            display: -webkit-box;
-            display: -webkit-flex;
-            display: -ms-flexbox;
-            display: flex;
-            min-height: 30px;
-        }
+        + ".text .string {"
+            + "border-bottom: 1px solid #eee;"
+            + "padding: 5px 0;"
+            + "display: -webkit-box;"
+            + "display: -webkit-flex;"
+            + "display: -ms-flexbox;"
+            + "display: flex;"
+            + "min-height: 30px;"
+        + "}"
 
-        .text .string.title {
-            text-align: center;
-            font-weight: 600;
-            font-size: 20px;
-        }
+        + ".text .string.title {"
+            + "text-align: center;"
+            + "font-weight: 600;"
+            + "font-size: 20px;"
+        + "}"
 
-        .text .string .original {
-            width: 50%;
-            padding: 0 10px;
-            border-right: 1px solid #eee;
-        }
+        + ".text .string .original {"
+            + "width: 50%;"
+            + "padding: 0 10px;"
+            + "border-right: 1px solid #eee;"
+        + "}"
 
-        .text .string .translate {
-            width: 50%;
-            padding: 0 10px;
-        }
+        + ".text .string .translate {"
+            + "width: 50%;"
+            + "padding: 0 10px;"
+        + "}"
 
-        .text .string .translate {
-            font-weight: 600;
-        }
-    </style>`);
+        + ".text .string .translate {"
+            + "font-weight: 600;"
+        + "}"
+    + "</style>");
     $iframe
         .contents()
         .find('body')
@@ -71,5 +71,5 @@ function openPrintDialogue() {
     window.frames['myiframe'].focus();
     window.frames['myiframe'].print();
 
-    setTimeout(() => { $(".printFrame").remove() }, 1000);
+    setTimeout(function() { return $(".printFrame").remove(); }, 1000);
 };
