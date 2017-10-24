c8e1506a8ff3489876964e413f55fc62416db2de
Alexander Kaminski <akaminski@dev-team.com>
2017-10-24 18:12:19 +0300

    Fixed bug with alt attribute on main page

--- BruteApp/BruteApp/Controllers/SearchSurfaceController.cs
+++ BruteApp/BruteApp/Controllers/SearchSurfaceController.cs
@@ -14,17 +14,9 @@ namespace BruteApp.Controllers
 {
     public class SearchSurfaceController : SurfaceController
     {
-        //public ActionResult Index()
-        //{
-        //    return CurrentUmbracoPage(); ;
-        //}
-
-
-
         [HttpGet]
         public JsonResult Search(string q = null)
         {
-            //var umbracoHelper = new UmbracoHelper(UmbracoContext.Current);
             var songs = Umbraco.TypedContentAtXPath("//song");
             var musicians = Umbraco.TypedContentAtXPath("//musician");
 
@@ -45,7 +37,6 @@ namespace BruteApp.Controllers
                 link = x.Url
             })
             .Where(x => x.text.ToLower().Contains(q))
-            .AsEnumerable()
             .OrderBy(x => x.text.ToLower().StartsWith(q) ? 0 : 1)
             .ThenBy(x => x.text)
             .Take(10)
@@ -58,73 +49,13 @@ namespace BruteApp.Controllers
                     link = x.Url
                 })
                 .Where(x => x.musician.ToLower().Contains(q))
-                .AsEnumerable()
                 .OrderBy(x => x.musician.ToLower().StartsWith(q) ? 0 : 1)
                 .ThenBy(x => x.musician)
                 .Take(5)
                 .ToList();
 
             musiciansResult.AddRange(songsResult);
-
-            //var songsList = GetSongsAsList(songs, q);
-
-            //var musicians = umbracoHelper.ContentAtXPath("//musician");
-            //var musiciansList = GetMusiciansAsList(musicians, q);
-
-            //musiciansList.AddRange(songs.ToList());
-
-            //foreach (var musician in musicians)
-            //{
-            //    var artist = new SearchModel
-            //    {
-            //        id = musician.Id,
-            //        text = musician.navigationName
-            //    };
-            //    if (artist.text.ToLower().Contains(q.ToLower()))
-            //    {
-            //        musiciansList.Add(artist);
-            //    }
-            //}
-
-            //songsList.AddRange(musiciansList);
-
             return Json(musiciansResult, JsonRequestBehavior.AllowGet);
         }
-
-        //private List<SearchModel> GetSongsAsList(dynamic dynamicItems, string q)
-        //{
-        //    var items = new List<SearchModel>();
-        //    foreach (var item in dynamicItems)
-        //    {
-        //        var itm = new SearchModel
-        //        {
-        //            id = item.Id,
-        //            text = item.Parent().NavigationName + " - " + item.SongName,
-        //            musician = item.Parent().NavigationName,
-        //            link = item.Url
-        //        };                
-        //        items.Add(itm);
-        //    }
-        //    return items;
-        //}
-
-        //private List<SearchModel> GetMusiciansAsList(dynamic dynamicItems, string q)
-        //{
-        //    var items = new List<SearchModel>();
-        //    foreach (var item in dynamicItems)
-        //    {
-        //        if (!item.NavigationName.ToLower().Contains(q.ToLower())) continue;
-        //        var itm = new SearchModel
-        //        {
-        //            id = item.Id,
-        //            text = item.NavigationName,
-        //            link = item.Url
-        //        };
-        //        items.Add(itm);
-        //    }
-        //    items = items.OrderBy(x => x.text).ToList();
-        //    return items;
-        //}
-
     }
 }
